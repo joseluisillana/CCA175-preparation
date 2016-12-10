4107b4eebb2689564a98e64bf043d1e0d3c5b54d
joseluisillana <joseluisillana@gmail.com>
2016-12-10 12:14:14 +0100

    Read from JSON

---
+++ 02_TransformSparkPython/readFromJSONinHDFS.py
@@ -0,0 +1,13 @@
+from pyspark import SparkContext, SparkConf, SqlContext
+conf = SparkConf().setAppName("pyspark-readFromJSONinHDFS-py")
+sc = SparkContext(conf=conf)
+sqlContext = SqlContext(sc)
+
+departmentsJson = sqlContext.jsonFile("/user/joseluisillana1709/departments_json/departments.json")
+departmentsJson.registerTempTable("departmentsTable")
+departmentsData = sqlContext.sql("select * from departmentsTable")
+for rec in departmentsData.collect():
+    print(rec)
+
+#Writing data in json format
+departmentsData.toJSON().saveAsTextFile("/user/joseluisillana1709/pruebas_spark/result/departmentsJson")
