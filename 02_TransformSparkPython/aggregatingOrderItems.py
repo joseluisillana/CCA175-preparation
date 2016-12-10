f8a6aa52c559222131074e25106f38e6e71daf3a
joseluisillana <joseluisillana@gmail.com>
2016-12-10 14:28:33 +0100

    aggregations with spark

--- 02_TransformSparkPython/aggregatingOrderItems.py
+++ 02_TransformSparkPython/aggregatingOrderItems.py
@@ -3,4 +3,4 @@ conf = SparkConf().setAppName("pyspark-aggregatingOrderItems-py")
 sc = SparkContext(conf=conf)
 
 ordersRDD = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/orders_jl")
-ordersRDD.count()
+print(ordersRDD.count())
