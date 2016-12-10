adf850a3ce0e99f946d88c78edec84fd3625e714
joseluisillana <joseluisillana@gmail.com>
2016-12-10 11:27:25 +0100

    Read steps

--- 02_TransformSparkPython/readSequenceFileWithKey.py
+++ 02_TransformSparkPython/readSequenceFileWithKey.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("pyspark-readSequenceFileWithoutKey-py")
 sc = SparkContext(conf=conf)
 
-dataRDD = sc.sequenceFile("/user/joseluisillana1709/pruebas_spark/result/departmentsWithKeyAsSF","org.apache.hadoop.io.IntWritable","org.apache.hadoop.io.TextWritable")
+dataRDD = sc.sequenceFile("/user/joseluisillana1709/pruebas_spark/result/departmentsWithKeyAsSF","org.apache.hadoop.io.IntWritable","org.apache.hadoop.io.Text")
 
 for rec in dataRDD.collect():
     print(rec)
