522ccd0f83733a86f8541f74d140629634caca49
joseluisillana <joseluisillana@gmail.com>
2016-12-10 14:35:40 +0100

    aggregations with spark

--- 02_TransformSparkPython/aggAVGRevenueTotalOrderItems.py
+++ 02_TransformSparkPython/aggAVGRevenueTotalOrderItems.py
@@ -3,13 +3,13 @@ conf = SparkConf().setAppName("pyspark-aggAVGRevenueTotalOrderItems-py")
 sc = SparkContext(conf=conf)
 
 revenue = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/order_items_jl").
-    map(lambda rec: float(rec.split(",")[4])).
-    reduce(lambda rev1, rev2: rev1 + rev2)
+map(lambda rec: float(rec.split(",")[4])).
+reduce(lambda rev1, rev2: rev1 + rev2)
 
 totalOrders = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/order_items_jl").
-    map(lambda rec: int(rec.split(",")[1])).
-    distinct().
-    count()
+map(lambda rec: int(rec.split(",")[1])).
+distinct().
+count()
 
 result = revenue/totalOrders
 
