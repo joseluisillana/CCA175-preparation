4ef1aed2beca83f313ae8073fe1cdfd6bbd4af1b
Jose Luis Illana Ruiz <joseluisillana@gmail.com>
2016-12-12 13:07:35 +0100

    Adding sorting layer

--- 02_TransformSparkPython/sortingAndRankingsortByKey_false.py
+++ 02_TransformSparkPython/sortingAndRankingsortByKey_false.py
@@ -3,7 +3,7 @@ conf = SparkConf().setAppName("pyspark-sortingAndRankingsortByKey_false-py")
 sc = SparkContext(conf=conf)
 
 ordersRAW = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/orders_jl")
-ordersMAP = ordersRAW.map(lambda rec: (int(rec.spli(",")[0]),rec))
+ordersMAP = ordersRAW.map(lambda rec: (int(rec.split(",")[0]),rec))
 ordersSorted = ordersMAP.sortByKey(false)
 
 for item in ordersSorted.take(5):
