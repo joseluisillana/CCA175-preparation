29824ec11e137057ef05636474417bbabbde7b1d
joseluisillana <joseluisillana@gmail.com>
2016-12-12 19:38:49 +0100

    End of python

--- 02_TransformSparkPython/sortingProductsByPriceEachCat.py
+++ 02_TransformSparkPython/sortingProductsByPriceEachCat.py
@@ -13,7 +13,7 @@ productsGroupBy = productsMap.groupByKey()
 for item in productsGroupBy.take(100):
 	print item
 
-for i in productsGroupBy.map(lambda rec: sorted(rec[1], key=lambda k: float(k.split(",")[4] if (k.split(",")[4] != "" and not " " in k.split(",")[4]) else "0"))).take(100):
+for i in productsGroupBy.map(lambda rec: sorted(rec[1], key=lambda k: float(k.split(",")[4] if (k.split(",")[4] != "" and not " " in k.split(",")[4]) else "12345.67"))).take(100):
 	print(i)
 
 for i in productsGroupBy.map(lambda rec: sorted(rec[1], key=lambda k: float(k.split(",")[4]), reverse=True)).take(100):
