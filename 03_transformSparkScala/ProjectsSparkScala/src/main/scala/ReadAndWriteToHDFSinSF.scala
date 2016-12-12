5d713987ca906eac261821e4c9895476389ded5e
joseluisillana <joseluisillana@gmail.com>
2016-12-12 23:30:34 +0100

    Scala part 1

--- 03_transformSparkScala/ProjectsSparkScala/src/main/scala/ReadAndWriteToHDFSinSF.scala
+++ 03_transformSparkScala/ProjectsSparkScala/src/main/scala/ReadAndWriteToHDFSinSF.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.hadoop.io._
+import org.apache.hadoop.io.NullWritable
 import org.apache.hadoop.mapreduce.lib.output._
 
 object ReadAndWriteToHDFSinSF {
@@ -9,7 +9,7 @@ object ReadAndWriteToHDFSinSF {
     val sc = new SparkContext(conf)
     val dataRDD = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/departments_jl")
 
-    val dataMap = dataRDD.map(x => (NullWritable,x))
+    val dataMap = dataRDD.map(x => (NullWritable.get(),x))
 
     dataMap.saveAsSequenceFile("/user/joseluisillana1709/pruebas_spark/sparkresults/departmentsSequenceFile")
   }
