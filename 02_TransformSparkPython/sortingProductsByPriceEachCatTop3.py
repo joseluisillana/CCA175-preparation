153adc4c3315c0cc0225fc251d172a4d3d0d7e37
Jose Luis Illana Ruiz <joseluisillana@gmail.com>
2016-12-12 19:01:03 +0000

    fix

--- 02_TransformSparkPython/sortingProductsByPriceEachCatTop3.py
+++ 02_TransformSparkPython/sortingProductsByPriceEachCatTop3.py
@@ -9,20 +9,23 @@ productsMap =  productsRAW.map(lambda rec: (int(rec.split(",")[1]), rec))
 
 productsGroupBy = productsMap.groupByKey()
 
-for i in productsGroupBy.flatMap(lambda x: getTopDenseN(x, 2)).collect():
-	print(i)
+#for i in productsGroupBy.flatMap(lambda x: getTopDenseN(x, 2)).collect():
+#	print(i)
 
 def getTopDenseN(rec, topN):
-	x = [ ]
-	topNPrices = [ ]
-	prodPrices = [ ]
+    x = [ ]
+    topNPrices = [ ]
+    prodPrices = [ ]
     prodPricesDesc = [ ]
     for i in rec[1]:
-      prodPrices.append(float(i.split(",")[4]))
-      prodPricesDesc = list(sorted(set(prodPrices), reverse=True))
+       prodPrices.append(float(i.split(",")[4]))
+       prodPricesDesc = list(sorted(set(prodPrices), reverse=True))
     import itertools
     topNPrices = list(itertools.islice(prodPricesDesc, 0, topN))
     for j in sorted(rec[1], key=lambda k: float(k.split(",")[4]), reverse=True):
-      if(float(j.split(",")[4]) in topNPrices):
-		  x.append(j)
-	return (y for y in x)
+        if(float(j.split(",")[4]) in topNPrices):
+            x.append(j)
+    return (y for y in x)
+
+for i in productsGroupBy.flatMap(lambda x: getTopDenseN(x, 2)).collect():
+    print(i)
