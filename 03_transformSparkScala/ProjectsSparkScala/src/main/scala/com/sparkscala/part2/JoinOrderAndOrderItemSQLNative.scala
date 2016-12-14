1d8df1acbed8fced770b28b656254da49d2b0570
Jose Luis Illana Ruiz <joseluisillana@gmail.com>
2016-12-14 16:34:40 +0100

    Part 2 scala spark query hive and native

--- 03_transformSparkScala/ProjectsSparkScala/src/main/scala/com/sparkscala/part2/JoinOrderAndOrderItemSQLNative.scala
+++ 03_transformSparkScala/ProjectsSparkScala/src/main/scala/com/sparkscala/part2/JoinOrderAndOrderItemSQLNative.scala
@@ -1,10 +1,26 @@
 package com.sparkscala.part2
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql._
 import org.apache.spark.{SparkConf, SparkContext}
+
+case class Orders(
+                   order_id: Int,
+                   order_date: String,
+                   order_customer_id: Int,
+                   order_status: String
+                 )
+case class OrderItems
+(order_item_id: Int,
+ order_item_order_id: Int,
+ order_item_product_id: Int,
+ order_item_quantity: Int,
+ order_item_subtotal: Float,
+ order_item_product_price: Float
+)
 
 object JoinOrderAndOrderItemSQLNative {
   def main(args: Array[String]) {
+
     val conf = new SparkConf().setAppName(s"${this.getClass.getName} with spark and scala")
     val sc = new SparkContext(conf)
 
@@ -17,29 +33,18 @@ object JoinOrderAndOrderItemSQLNative {
     val ordersMap = ordersRDD.
       map(o => o.split(","))
 
-    case class Orders(
-                       order_id: Int,
-                       order_date: String,
-                       order_customer_id: Int,
-                       order_status: String
-                     )
+
 
     val orders = ordersMap.map(o => Orders(o(0).toInt, o(1), o(2).toInt, o(3)))
 
     import sqlContext.createSchemaRDD
+
     orders.registerTempTable("orders")
 
     val orderItemsRDD = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/order_items_jl")
     val orderItemsMap = orderItemsRDD.map(oi => oi.split(","))
 
-    case class OrderItems
-    (order_item_id: Int,
-     order_item_order_id: Int,
-     order_item_product_id: Int,
-     order_item_quantity: Int,
-     order_item_subtotal: Float,
-     order_item_product_price: Float
-    )
+
 
     val orderItems = sc.textFile("/user/joseluisillana1709/pruebas_spark/raw/sqoop_import/order_items_jl").
       map(rec => rec.split(",")).
